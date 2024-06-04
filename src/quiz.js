ef06543dd1d10005b18a9e7aad220185146b6fab
caseyskat <100611953+caseyskat@users.noreply.github.com>
2024-06-04 20:34:46 +0300

    done

--- src/quiz.js
+++ src/quiz.js
@@ -2,33 +2,33 @@ const questions = [
   {
     question: <img src="assets/1.jpg" alt=" " />,
     answers: [
-      { answerText: "smile", isCorrect: false },
+      { answerText: "Smile", isCorrect: false },
       { answerText: "bullet in the mouth", isCorrect: false },
-      { answerText: "bite the bullet", isCorrect: true },
+      { answerText: "Bite the bullet", isCorrect: true },
     ],
   },
   {
     question: <img src="assets/2.jpg" alt=" " />,
     answers: [
-      { answerText: "make a call", isCorrect: false },
-      { answerText: "call it a day", isCorrect: true },
-      { answerText: "say hello", isCorrect: false },
+      { answerText: "Make a call", isCorrect: false },
+      { answerText: "Call it a day", isCorrect: true },
+      { answerText: "Say hello", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/3.jpg" alt=" " />,
     answers: [
-      { answerText: "bare winter", isCorrect: false },
-      { answerText: "have a cold feet", isCorrect: true },
-      { answerText: "have ice feet", isCorrect: false },
+      { answerText: "Bare winter", isCorrect: false },
+      { answerText: "Have a cold feet", isCorrect: true },
+      { answerText: "Have ice feet", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/4.jpg" alt=" " />,
     answers: [
-      { answerText: "cut the corners", isCorrect: true },
-      { answerText: "cut the sticker", isCorrect: false },
-      { answerText: "paper cut", isCorrect: false },
+      { answerText: "Cut the corners", isCorrect: true },
+      { answerText: "Cut the sticker", isCorrect: false },
+      { answerText: "Paper cut", isCorrect: false },
     ],
   },
   {
@@ -44,7 +44,7 @@ const questions = [
     answers: [
       { answerText: "room in the elephant", isCorrect: false },
       { answerText: "thinking man", isCorrect: false },
-      { answerText: "elephant in the room", isCorrect: true },
+      { answerText: "Elephant in the room", isCorrect: true },
     ],
   },
   {
@@ -76,95 +76,127 @@ const questions = [
     answers: [
       { answerText: "piece of cake", isCorrect: true },
       { answerText: "tasty cake", isCorrect: false },
-      { answerText: "cake of peace", isCorrect: false },
+      { answerText: "peace of cake ", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/11.jpg" alt=" " />,
     answers: [
-      { answerText: "coockie clever", isCorrect: false },
-      { answerText: "reading coockie", isCorrect: false },
-      { answerText: "smart coockie", isCorrect: true },
+      { answerText: "learning cookie", isCorrect: false },
+      { answerText: "smart cookie", isCorrect: true },
+      { answerText: "be smart", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/12.jpg" alt=" " />,
     answers: [
-      { answerText: "pig queen", isCorrect: false },
-      { answerText: "see the big in the sky", isCorrect: false },
+      { answerText: "flying pig", isCorrect: false },
+      { answerText: "peppa fly", isCorrect: false },
       { answerText: "when the pigs fly", isCorrect: true },
     ],
   },
   {
     question: <img src="assets/13.jpg" alt=" " />,
     answers: [
-      { answerText: "devil speak to a men", isCorrect: false },
       { answerText: "speak to the devil", isCorrect: true },
-      { answerText: "visiting hell", isCorrect: false },
+      { answerText: "find the devil", isCorrect: false },
+      { answerText: "visit the devil", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/14.jpg" alt=" " />,
     answers: [
-      { answerText: "kill two birds with a stone", isCorrect: true },
-      { answerText: "make the birds sleep", isCorrect: false },
       { answerText: "dead birds", isCorrect: false },
+      { answerText: "kill two birds with a stone", isCorrect: true },
+      { answerText: "two birds better then a stone", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/15.jpg" alt=" " />,
     answers: [
-      { answerText: "be a potato chip", isCorrect: false },
+      { answerText: "potato relax", isCorrect: false },
       { answerText: "couch potato", isCorrect: true },
-      { answerText: "vegetable couch", isCorrect: false },
+      { answerText: "chill potato", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/16.jpg" alt=" " />,
     answers: [
-      { answerText: "be a cool sheep", isCorrect: false },
+      { answerText: "funny sheep", isCorrect: false },
+      { answerText: "be a sheep", isCorrect: false },
       { answerText: "to be a black sheep", isCorrect: true },
-      { answerText: "to be a white sheep", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/17.jpg" alt=" " />,
     answers: [
-      { answerText: "make the long story short", isCorrect: true },
+      { answerText: "make a short story long", isCorrect: false },
       { answerText: "make up a story", isCorrect: false },
-      { answerText: "write a story", isCorrect: false },
+      { answerText: "make a long story short", isCorrect: true },
     ],
   },
   {
     question: <img src="assets/18.jpg" alt=" " />,
     answers: [
-      { answerText: "break a bear", isCorrect: false },
       { answerText: "break a leg", isCorrect: true },
-      { answerText: "fix your taddy", isCorrect: false },
+      { answerText: "broken leg", isCorrect: false },
+      { answerText: "sick taddy", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/19.jpg" alt=" " />,
     answers: [
-      { answerText: "take a straw", isCorrect: false },
       { answerText: "the last straw", isCorrect: true },
-      { answerText: "get some drink", isCorrect: false },
+      { answerText: "straw in a drink", isCorrect: false },
+      { answerText: "take a straw", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/20.jpg" alt=" " />,
     answers: [
       { answerText: "money cow", isCorrect: false },
-      { answerText: "milky cash", isCorrect: false },
       { answerText: "cash cow", isCorrect: true },
+      { answerText: "make money from a cow", isCorrect: false },
     ],
   },
   {
     question: <img src="assets/21.jpg" alt=" " />,
     answers: [
-      { answerText: "pull your socks up", isCorrect: true },
-      { answerText: "get your socks", isCorrect: false },
+      { answerText: "wear your socks", isCorrect: false },
+      { answerText: "pull the socks up", isCorrect: true },
       { answerText: "get dressed", isCorrect: false },
+    ],
+  },
+  {
+    question: <img src="assets/22.jpg" alt=" " />,
+    answers: [
+      { answerText: "warm up your tea", isCorrect: false },
+      { answerText: "make a storm in a teacup", isCorrect: true },
+      { answerText: "thunder in a cup", isCorrect: false },
+    ],
+  },
+  {
+    question: <img src="assets/23.jpg" alt=" " />,
+    answers: [
+      { answerText: "candy on my tongue", isCorrect: false },
+      { answerText: "sweet tongue", isCorrect: false },
+      { answerText: "on the tip of my tongue", isCorrect: true },
+    ],
+  },
+  {
+    question: <img src="assets/24.jpg" alt=" " />,
+    answers: [
+      { answerText: "wet my mind", isCorrect: false },
+      { answerText: "wash your brain", isCorrect: false },
+      { answerText: "slipped my mind", isCorrect: true },
+    ],
+  },
+  {
+    question: <img src="assets/25.jpg" alt=" " />,
+    answers: [
+      { answerText: "whispering a secret", isCorrect: false },
+      { answerText: "things go in ear and out the other", isCorrect: true },
+      { answerText: "going out of my ears", isCorrect: false },
     ],
   },
 ];
